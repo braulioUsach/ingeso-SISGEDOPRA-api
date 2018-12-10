9451f2045c420c6e102414f6a00fdf6dc558c8b7
Braulio Castro (Chile) <Braulio.Castro@walmart.com>
2018-12-09 21:55:26 -0300

    feat(transfer): allow transfer when user is the creator

--- src/api/transfer/index.js
+++ src/api/transfer/index.js
@@ -1,15 +1,22 @@
 const Repository = require('./repository');
+const DocumentRepository = require('../document/repository');
 const Helper = require('./helper');
 
 class Transfer {
   static create(params, tokenValues) {
     return new Promise((resolve, reject) => {
       const paramsFormatted = Helper.paramsToCreate(params, tokenValues);
-      return Repository.readLastByDocument(params.document)
-        .then((row) => {
-          if (row.userIdTo !== tokenValues.userId) {
+      const q = [];
+      q.push(Repository.readLastByDocument(params.document));
+      q.push(DocumentRepository.read(params.document));
+      return Promise.all(q)
+        .then((promises) => {
+          const isCreator = promises[1].creatorId === tokenValues.userId;
+          const hasTransfers = !(promises[0].length === 0);
+          if (!isCreator && (!hasTransfers || promises[0].userIdTo !== tokenValues.userId)) {
             reject(new Error('No puedes transferir el documento, ya que no eres el actual responsable del documento'));
           }
+
           return Repository.create(paramsFormatted);
         })
         .then((row) => {
