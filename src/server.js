d09e04db06114d123da067082d52a31e82621045
Braulio Castro (Chile) <Braulio.Castro@walmart.com>
2018-12-10 23:50:18 -0300

    refactor(server): add message when start up with healthy check

--- src/server.js
+++ src/server.js
@@ -2,4 +2,7 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`API server started on ${port}`));
+app.listen(port, () => {
+  console.info(`API server started on http://localhost:${port}`);
+  console.info(`Status on: http://localhost:${port}/health`);
+});
